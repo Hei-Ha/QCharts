e63155f74f8dc0f28fc05d3c02d92aad9b924e35
wangchuang <wangchuang@gmial.com>
2022-11-02 19:16:28 +0800

    ..

--- src/menu/menu.tsx
+++ src/menu/menu.tsx
@@ -32,9 +32,6 @@ export const BaseMenu = () => {
                 Link to text
             </MenuItem>
         </Link>
-
-
-
     </Menu>
 
 }
--- src/views/chartsExample/columnCharts/ColumnChartsContent.tsx
+++ src/views/chartsExample/columnCharts/ColumnChartsContent.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Grid } from '@arco-design/web-react';
+
+const Row = Grid.Row;
+const Col = Grid.Col;
 
 export const ColumnChartsContent = () => {
-    return <div>柱状图例子</div>
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    return <div className='flex flex-wrap w-calc[100%-300px]'>
+        {arr.map((item) => {
+            return <div style={{ width: '700px', height: '50px' }} className='bg-#CCCCCC border border-solid border-#CCCCCC mb-2'>{item}</div>
+        })}
+    </div>
 }
