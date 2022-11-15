4ddd3ebf73fe8a8e71664085695e3d73567be3b5
wangchuang <wangchuang@gmial.com>
2022-11-15 19:33:22 +0800

    test

--- src/views/chartsExample/columnCharts/index.tsx
+++ src/views/chartsExample/columnCharts/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@arco-design/web-react';
+import { Grid, Button } from '@arco-design/web-react';
 import {Link} from "react-router-dom";
 
 import { BaseColumnChart } from '@src/views/chartsExample/columnCharts/components/BaseColumnChart';
@@ -10,16 +10,9 @@ const Col = Grid.Col;
 export const Index = () => {
     return <div>
         <Link to='/charts/columnCharts/base'>
-            基础柱状图
+            <Button type='primary'>
+                基础柱状图
+            </Button>
         </Link>
     </div>
-    // const arr = [1, 2, 3, 4, 5, 6, 7];
-    // return <div className='flex flex-wrap justify-between w-calc[100%-300px]'>
-    //     <div>
-    //
-    //     </div>
-    //     {/*{arr.map((item) => {*/}
-    //     {/*    return <div style={{ width: '700px', height: '50px' }} className='bg-#CCCCCC border border-solid border-#CCCCCC mb-2'>{item}</div>*/}
-    //     {/*})}*/}
-    // </div>
 }
